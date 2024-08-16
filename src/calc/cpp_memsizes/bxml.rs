21032f610b06c06c7aff6d07fc03f7430f472e7a
GingerAvalanche <chodness@gmail.com>
2024-08-16 08:16:10 -0700

    Add switch overhead for bxml

--- src/calc/cpp_memsizes/bxml.rs
+++ src/calc/cpp_memsizes/bxml.rs
@@ -9,11 +9,12 @@ const CLASS_SIZE_NX: usize = std::mem::size_of::<ActorLink<u64>>();
 const TAG_SIZE: usize = std::mem::size_of::<u32>();
 
 const OVERHEAD_WIIU: usize = 0x64;
+const OVERHEAD_NX: usize = 0x44;
 
 pub fn parse_size(bytes: &[u8], endian: Endian) -> Option<u32> {
     let mut total_size: usize = match endian {
         Endian::Big => super::PARSE_CONST_WIIU + CLASS_SIZE_WIIU + OVERHEAD_WIIU,
-        Endian::Little => super::PARSE_CONST_NX + CLASS_SIZE_NX,
+        Endian::Little => super::PARSE_CONST_NX + CLASS_SIZE_NX + OVERHEAD_NX,
     };
     let a = ParameterIO::from_binary(bytes).ok()?;
 
