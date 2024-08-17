e3a201346513d9e2655a6840be3e50e6b2f4e7f8
GingerAvalanche <chodness@gmail.com>
2024-08-16 23:30:19 -0700

    Solved LifeCondition on WiiU
    Switch, go figure, needs testing

--- src/calc/cpp_memsizes/blifecondition.rs
+++ src/calc/cpp_memsizes/blifecondition.rs
@@ -6,7 +6,8 @@ use super::cpp_classes::{agl, sead, LifeCondition::LifeCondition};
 const CLASS_SIZE_WIIU: usize = std::mem::size_of::<LifeCondition<u32>>();
 const CLASS_SIZE_NX: usize = std::mem::size_of::<LifeCondition<u64>>();
 
-const OVERHEAD_WIIU: usize = 0x88;
+// TODO: Why does this still have unaccounted-for overhead?
+const OVERHEAD_WIIU: usize = 0x40;
 const OVERHEAD_NX: usize = 0x0;
 
 pub fn parse_size(bytes: &[u8], endian: Endian) -> Option<u32> {
@@ -14,24 +15,28 @@ pub fn parse_size(bytes: &[u8], endian: Endian) -> Option<u32> {
         Endian::Big => super::PARSE_CONST_WIIU + CLASS_SIZE_WIIU + OVERHEAD_WIIU,
         Endian::Little => super::PARSE_CONST_NX + CLASS_SIZE_NX + OVERHEAD_NX,
     };
-    let safestring_size = match endian {
-        Endian::Big => size_of::<agl::Parameter<u32, sead::SafeString<u32>>>() + 0x2,
-        Endian::Little => size_of::<agl::Parameter<u64, sead::SafeString<u64>>>(),
-    };
+    let (
+        iter_size,
+        safestring_size,
+    );
+    match endian {
+        Endian::Big => {
+            iter_size = super::ITER_CONST_WIIU;
+            safestring_size = size_of::<agl::Parameter<u32, sead::SafeString<u32>>>();
+        }
+        Endian::Little => {
+            iter_size = super::ITER_CONST_NX;
+            safestring_size = size_of::<agl::Parameter<u64, sead::SafeString<u64>>>();
+        }
+    }
 
     let a = ParameterIO::from_binary(bytes).ok()?;
 
-    total_size += a.param_root.objects.iter()
-        .filter_map(|(k, v)| {
-            match k.hash() {
-                3730121527 |
-                1959350547 |
-                1390640162 |
-                1118737566 => Some(v.0.len()),
-                _ => None,
-            }
-        })
-        .sum::<usize>() * safestring_size;
+    for key in ["InvalidWeathers", "InvalidTimes", "DeleteWeathers", "DeleteTimes"] {
+        if let Some(object) = a.param_root.objects.get(key) {
+            total_size += iter_size + object.len() * safestring_size;
+        }
+    }
 
     Some(total_size as u32)
 }
