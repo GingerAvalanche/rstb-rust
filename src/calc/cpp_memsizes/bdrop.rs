a4044e30406ed1e0a7782025b2b914b11c678273
GingerAvalanche <chodness@gmail.com>
2024-08-16 22:41:03 -0700

    Drop solved on WiiU
    Switch still in testing

--- src/calc/cpp_memsizes/bdrop.rs
+++ src/calc/cpp_memsizes/bdrop.rs
@@ -8,8 +8,10 @@ use crate::Endian;
 const CLASS_SIZE_WIIU: usize = std::mem::size_of::<Drop<u32>>();
 const CLASS_SIZE_NX: usize = std::mem::size_of::<Drop<u64>>();
 
-const OVERHEAD_WIIU: usize = 0x310;
+const OVERHEAD_WIIU: usize = 0x0;
 const OVERHEAD_NX: usize = 0x0;
+const HEADER_OVERHEAD_WIIU: usize = 0x20;
+const HEADER_OVERHEAD_NX: usize = 0x0;
 
 pub fn parse_size(bytes: &[u8], endian: Endian) -> Option<u32> {
     let mut total_size = match endian {
@@ -18,27 +20,43 @@ pub fn parse_size(bytes: &[u8], endian: Endian) -> Option<u32> {
     };
 
     let a = ParameterIO::from_binary(bytes).ok()?;
-    let (table_size, item_size): (usize, usize);
+    let (
+        iter_size,
+        header_size,
+        table_size,
+        item_size,
+    );
     match endian {
         Endian::Big => {
+            iter_size = super::ITER_CONST_WIIU;
+            header_size = HEADER_OVERHEAD_WIIU;
             table_size = size_of::<Table<u32>>();
             item_size = size_of::<Item<u32>>();
         }
         Endian::Little => {
+            iter_size = super::ITER_CONST_NX;
+            header_size = HEADER_OVERHEAD_NX;
             table_size = size_of::<Table<u64>>();
             item_size = size_of::<Item<u64>>();
         }
     }
 
     if let Some(header) = a.param_root.objects.get("Header") {
+        total_size += header_size;
         if let Some(num_tables_param) = header.get("TableNum") {
             let num_tables: usize = num_tables_param.as_int().ok()?;
+            if num_tables > 0 {
+                total_size += iter_size;
+            }
             total_size += num_tables * table_size;
             for i in 0..num_tables {
                 let table_id = format!("Table{:02}", i + 1);
                 let table_name = header.get(table_id)?.as_string64().ok()?.as_str();
                 if let Some(table) = a.param_root.objects.get(table_name) {
                     let num_items: usize = table.get("ColumnNum")?.as_int().ok()?;
+                    if num_items > 0 {
+                        total_size += iter_size;
+                    }
                     total_size += num_items * item_size;
                 }
             }
