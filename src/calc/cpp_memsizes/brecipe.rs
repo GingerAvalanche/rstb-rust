01927106076b220ab1f6c895c006fd1ac6c324cc
GingerAvalanche <chodness@gmail.com>
2024-08-16 20:03:48 -0700

    Solved Recipe on WiiiU
    Switch is under testing

--- src/calc/cpp_memsizes/brecipe.rs
+++ src/calc/cpp_memsizes/brecipe.rs
@@ -8,8 +8,12 @@ use crate::Endian;
 const CLASS_SIZE_WIIU: usize = std::mem::size_of::<Recipe<u32>>();
 const CLASS_SIZE_NX: usize = std::mem::size_of::<Recipe<u64>>();
 
-const OVERHEAD_WIIU: usize = 0x60;
+const OVERHEAD_WIIU: usize = 0x0;
 const OVERHEAD_NX: usize = 0x0;
+const HEADER_OVERHEAD_WIIU: usize = 0x20;
+const HEADER_OVERHEAD_NX: usize = 0x0;
+const TABLES_OVERHEAD_WIIU: usize = 0x20;
+const TABLES_OVERHEAD_NX: usize = 0x0;
 
 pub fn parse_size(bytes: &[u8], endian: Endian) -> Option<u32> {
     let mut total_size = match endian {
@@ -18,21 +22,34 @@ pub fn parse_size(bytes: &[u8], endian: Endian) -> Option<u32> {
     };
 
     let a = ParameterIO::from_binary(bytes).ok()?;
-    let (table_size, item_size): (usize, usize);
+    let (
+        header_overhead,
+        table_overhead,
+        table_size,
+        item_size,
+    );
     match endian {
         Endian::Big => {
+            header_overhead = HEADER_OVERHEAD_WIIU;
+            table_overhead = TABLES_OVERHEAD_WIIU;
             table_size = size_of::<Table<u32>>();
             item_size = size_of::<Item<u32>>();
         }
         Endian::Little => {
+            header_overhead = HEADER_OVERHEAD_NX;
+            table_overhead = TABLES_OVERHEAD_NX;
             table_size = size_of::<Table<u64>>();
             item_size = size_of::<Item<u64>>();
         }
     }
 
     if let Some(header) = a.param_root.objects.get("Header") {
+        total_size += header_overhead;
         if let Some(num_tables_param) = header.get("TableNum") {
             let num_tables: usize = num_tables_param.as_int().ok()?;
+            if num_tables > 0 {
+                total_size += table_overhead;
+            }
             total_size += num_tables * table_size;
             for i in 0..num_tables {
                 let table_id = format!("Table{:02}", i + 1);
